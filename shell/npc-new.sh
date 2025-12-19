52b4fbc9b3d3456195ff144d10a3888ba3d7f0e7
文迪 <wendy8wendy@hotmail.com>
2025-12-20 04:22:36 +0800

    Add new shell script npc-new.sh

--- shell/npc-new.sh
+++ shell/npc-new.sh
@@ -24,7 +24,7 @@ echo -e "
 \033[0;1;31m#-------------------------------------------------------------------------------#
 \033[0;1;32m#-------------------------------------------------------------------------------#
  "
-wget --no-check-certificate --header 'Content-Type: application/json;charset=UTF-8' -qO npc.tar.gz 'https://gitee.com/api/v5/repos/wendy-tools/get/raw/nps/v0.26.9/client/linux_amd64_client.tar.gz?access_token=e714ea221d82ae604117e8410d2d309e'
+wget --no-check-certificate -O npc.tar.gz 'https://github.com/ehang-io/nps/releases/download/v0.26.10/linux_amd64_client.tar.gz'
 echo -e "
 \033[0;1;30m#-------------------------------------------------------------------------------#
 \033[0;1;31m#-------------------------------------------------------------------------------#
